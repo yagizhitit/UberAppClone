0150b3f3c02b6536b13d1b86eb62bfa56b4c9851
Yagiz Hitit <yagizhitit@gmail.com>
2023-12-06 22:08:31 +0000

    Initial Commit

---
+++ UberAppClone/UberAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberAppCloneApp.swift
+//  UberAppClone
+//
+//  Created by Yağız Hitit on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
