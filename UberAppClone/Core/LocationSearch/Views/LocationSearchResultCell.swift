3972a5902458bdd6275bdc794198ea9c0e94895d
Yagiz Hitit <yagizhitit@gmail.com>
2023-12-10 14:14:28 +0100

    Location Search View and Location Cells

---
+++ UberAppClone/Core/LocationSearch/Views/LocationSearchResultCell.swift
@@ -0,0 +1,38 @@
+//
+//  LocationSearchResultCell.swift
+//  UberAppClone
+//
+//  Created by Yağız Hitit on 10.12.2023.
+//
+
+import SwiftUI
+
+struct LocationSearchResultCell: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "mappin.circle.fill")
+                .resizable()
+                .foregroundColor(.blue)
+                .accentColor(.white)
+                .frame(width: 40, height: 40)
+            
+            VStack(alignment: .leading, spacing: 4){
+                Text("Starbucks Coffee")
+                    .font(.body)
+                
+                Text("123 Main St, Cupertino CA")
+                    .font(.system(size: 15))
+                    .foregroundColor(.gray)
+                
+                Divider()
+            }
+            .padding(.leading, 8)
+            .padding(.vertical, 8)
+        }
+        .padding(.leading)
+    }
+}
+
+#Preview {
+    LocationSearchResultCell()
+}
