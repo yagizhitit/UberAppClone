d6a44cc6fa9b777227ce38a330e8f9f8838ed2dd
Yagiz Hitit <yagizhitit@gmail.com>
2023-12-10 14:28:32 +0100

    Location Search UI, animations

--- UberAppClone/Core/Home/Views/HomeView.swift
+++ UberAppClone/Core/Home/Views/HomeView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showLocationSearchView = false
     var body: some View {
         ZStack(alignment: .top) {
             UberMapViewRepresentable()
                 .ignoresSafeArea()
             
-            LocationSearchActivationView()
-                .padding(.top, 72)
+            if showLocationSearchView{
+                LocationSearchView()
+            } else{
+                LocationSearchActivationView()
+                    .padding(.top, 72)
+                    .onTapGesture(perform: {
+                        withAnimation(.spring()){
+                            showLocationSearchView.toggle()
+                        }
+                    })
+            }
             
-            MapViewActionButton()
+            MapViewActionButton(showLocationSearchView: $showLocationSearchView)
                 .padding(.leading)
                 .padding(.top, 4)
         }
