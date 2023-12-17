31d827a52e04a13f82f87bec1f189d28f182a43c
Yagiz Hitit <yagizhitit@gmail.com>
2023-12-17 20:38:27 +0000

    Showing Ride Request View on Home View and selected route appearance changed

--- UberAppClone/Core/Trips/RideRequestView.swift
+++ UberAppClone/Core/Trips/RideRequestView.swift
@@ -13,6 +13,7 @@ struct RideRequestView: View {
             Capsule()
                 .foregroundColor(Color(.systemGray5))
                 .frame(width: 48, height: 6)
+                .padding(.top, 8)
             
             //trip info view
             HStack {
@@ -136,7 +137,9 @@ struct RideRequestView: View {
                     .padding(.top, 7)
             }
         }
+        .padding(.bottom, 24)
         .background(.white)
+        .cornerRadius(16)
     }
 }
 
