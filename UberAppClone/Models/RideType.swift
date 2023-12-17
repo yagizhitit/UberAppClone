5a15c974478a6c440b24473cb80b03b8a652b376
Yagiz Hitit <yagizhitit@gmail.com>
2023-12-17 21:19:09 +0000

    Ride Type Data Model

---
+++ UberAppClone/Models/RideType.swift
@@ -0,0 +1,32 @@
+//
+//  RideType.swift
+//  UberAppClone
+//
+//  Created by Yağız Hitit on 17.12.2023.
+//
+
+import Foundation
+
+enum RideType: Int, CaseIterable, Identifiable{
+    case uberX
+    case black
+    case uberXL
+    
+    var id: Int { return rawValue }
+    
+    var description: String{
+        switch self{
+        case .uberX: return "UberX"
+        case .black: return "UberBlack"
+        case .uberXL: return "UberXL"
+        }
+    }
+    
+    var imageName: String{
+        switch self{
+        case .uberX: return "uber-x"
+        case .black: return "uber-black"
+        case .uberXL: return "uber-x"
+        }
+    }
+}
