ad353e90e3671524029f891bd96f09053fc5064b
Yagiz Hitit <yagizhitit@gmail.com>
2023-12-18 22:09:25 +0000

    Adding informations on the ride request screen

--- UberAppClone/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberAppClone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -25,7 +25,6 @@ struct UberMapViewRepresentable: UIViewRepresentable{
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        print("DEBUG: Map state is \(mapState)")
         
         switch mapState{
         case .noInput:
@@ -34,10 +33,11 @@ struct UberMapViewRepresentable: UIViewRepresentable{
         case .searchingForLocation:
             break
         case.locationSelected:
-            if let coordinate = locationViewModel.selectedLocationCoordinate{
+            if let coordinate = locationViewModel.selectedUberLocation?.coordinate{
                 context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
                 context.coordinator.configurePollyline(withDestinationCoordinate: coordinate)
             }
+            break
         }
         
 //        if mapState == .noInput{
@@ -95,31 +95,11 @@ extension UberMapViewRepresentable{
         
         func configurePollyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D){
             guard let userLocationCoordinate = self.userLocationCoordinate else { return }
-            getDestinationRoute(from: userLocationCoordinate, to: coordinate) { route in
+            parent.locationViewModel.getDestinationRoute(from: userLocationCoordinate, to: coordinate) { route in
                 self.parent.mapView.addOverlay(route.polyline)
                 let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect,
                                                                edgePadding: .init(top: 64, left: 32, bottom: 500, right: 32))
                 self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
-            }
-        }
-        
-        func getDestinationRoute(from userLocation: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, completion: @escaping(MKRoute) -> Void){
-            let userPlacemark = MKPlacemark(coordinate: userLocation)
-            let destPlacemark = MKPlacemark(coordinate: destination)
-            let request = MKDirections.Request()
-            
-            request.source = MKMapItem(placemark: userPlacemark)
-            request.destination = MKMapItem(placemark: destPlacemark)
-            
-            let directions = MKDirections(request: request)
-            directions.calculate { response, error in
-                if let error = error{
-                    print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
-                    return
-                }
-                
-                guard let route = response?.routes.first else { return }
-                completion(route)
             }
         }
         
---
+++ UberAppClone/Models/UberLocation.swift
@@ -0,0 +1,14 @@
+//
+//  UberLocation.swift
+//  UberAppClone
+//
+//  Created by Yağız Hitit on 18.12.2023.
+//
+
+import Foundation
+import CoreLocation
+
+struct UberLocation{
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
