87c8724236ec258fd00c1a2823cd772e3d3a45a0
Yagiz Hitit <yagizhitit@gmail.com>
2023-12-18 22:19:37 +0000

    Fixed the issue of adding the polyline repeatedly

--- UberAppClone/Core/Home/Views/MapViewActionButton.swift
+++ UberAppClone/Core/Home/Views/MapViewActionButton.swift
@@ -32,7 +32,7 @@ struct MapViewActionButton: View {
             print("DEBUG: No input")
         case .searchingForLocation:
             mapState = .noInput
-        case .locationSelected:
+        case .locationSelected, .polylineAdded:
             mapState = .noInput
             viewModel.selectedUberLocation = nil
         }
@@ -44,6 +44,8 @@ struct MapViewActionButton: View {
             return "line.3.horizontal"
         case .searchingForLocation, .locationSelected:
             return "arrow.left"
+        default:
+            return "line.3.horizontal"
         }
     }
 }
--- UberAppClone/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberAppClone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -38,6 +38,8 @@ struct UberMapViewRepresentable: UIViewRepresentable{
                 context.coordinator.configurePollyline(withDestinationCoordinate: coordinate)
             }
             break
+        case .polylineAdded:
+            break
         }
         
 //        if mapState == .noInput{
@@ -97,6 +99,7 @@ extension UberMapViewRepresentable{
             guard let userLocationCoordinate = self.userLocationCoordinate else { return }
             parent.locationViewModel.getDestinationRoute(from: userLocationCoordinate, to: coordinate) { route in
                 self.parent.mapView.addOverlay(route.polyline)
+                self.parent.mapState = .polylineAdded
                 let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect,
                                                                edgePadding: .init(top: 64, left: 32, bottom: 500, right: 32))
                 self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
